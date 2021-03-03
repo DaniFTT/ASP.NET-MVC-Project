2c65c8713e12a103e55dd5b02e5ae23d139eeef1
DaniFTT <daniel.s.6140@gmail.com>
2021-03-03 16:32:25 -0300

    First Commit

---
+++ MVC Projec/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using MVC_Projec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Projec.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Book> Books;
+
+        public HomeController()
+        {
+            if (Books == null)
+                Books = new Book().GetBooks();
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(Books);
+        }
+
+        public ActionResult NovoLivro()
+        {
+            return View();
+        }
+
+        public ActionResult EditarLivro(int id)
+        {
+            return View(Books.FirstOrDefault(x => x.ID == id));
+        }
+
+        [HttpPost]
+        public ActionResult InserirLivro(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!Books.Any(x => x.ID == book.ID))
+                    Books.Add(book);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("NovoLivro", book);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AtualizarLivro(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                var index = Books.FindIndex(x => x.ID == book.ID);
+                Books[index] = book;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("EditarLivro", book);
+            }
+        }
+
+        public ActionResult ExcluirLivro(int id)
+        {
+            Books.Remove(Books.FirstOrDefault(x => x.ID == id));
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC Projec/Models/Book.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Projec.Models
+{
+    public class Book
+    {
+        [Required(ErrorMessage = "O código é obrigatorio")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatorio")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "O autor é obrigatorio")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "O ano de publicação é obrigatorio")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "O ano deve possuir 4 digitos")]
+        public int YearOfPublication { get; set; }
+
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    ID = 1,
+                    Title = "Domain Driven Design",
+                    Author = "Eric Evans",
+                    YearOfPublication = 2003
+                },
+                new Book
+                {
+                    ID = 2,
+                    Title = "Agile Principles, Patterns, and practices in c#",
+                    Author = "Robert C. Marim",
+                    YearOfPublication = 2006
+                },
+                new Book
+                {
+                    ID = 3,
+                    Title = "Clean Code",
+                    Author = "Robert C. Marim",
+                    YearOfPublication = 2008
+                },
+            };
+        }
+    }
+}
